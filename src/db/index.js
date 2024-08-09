17c9a2dcd45e1bce55ddc22ad4d01fa583994f70
rohitsharma159 <rs9599173169@gmail.com>
2024-08-09 16:35:26 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const ConnectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/$
+            {DB_NAME}`)
+            console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+            
+    } catch (error) {
+        console.error("error aa gya bhai", error)
+    }
+    process.exit(1);
+}
+
+
+export default ConnectDB
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv"
+import ConnectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+ConnectDB();
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}/$
+            {DB_NAME}`)
+            app.on("error", (error)=>{
+                console.error("error", error);
+                throw error
+            })
+            app.listen(process.env.PORT, ()=>{
+                console.log(`Server is running on port ${process.env.PORT}`);
+            });
+
+    } catch (error) {
+        console.error("ERROR", error)
+        throw err
+        
+    }
+                
+})()
+
+*/
