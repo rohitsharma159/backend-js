b425c8ae635f6436fde93245b3d8fa02b68002f5
rohitsharma159 <rs9599173169@gmail.com>
2024-08-19 12:42:40 +0530

    add user controler or bug fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,26 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/user.controller.js"
+import {upload} from "../middlewares/multer.middlewares.js"
 
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avtar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    
+    
+    
+    registerUser
+)
 
 
 export default router
